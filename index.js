467d3b90f8afcbc6f0b51777a37402aa1114eb97
Anup <anuptigga21@gmail.com>
2025-04-10 10:18:23 +0530

    fix:cors

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const adminRoutes = require('./routes/adminRoutes');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/colleges', collegeRoutes);
